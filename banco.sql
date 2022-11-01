66f0aeac967be473703e9e20fd434e775b56f3a1
ney333 <75282628+ney333@users.noreply.github.com>
2022-11-01 20:39:03 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -9,22 +9,22 @@ CREATE TABLE `clientes` (
   `cpf` int(11) NOT NULL,
   `rg` int(9) NOT NULL,
   `atribuicoes` varchar(300) NOT NULL,
-  `dt_inicio` DATE NOT NULL,
-  `dt_nasc` NOT NULL,
+  `dt_inicio` datetime NOT NULL,
+  `dt_nasc` datetime NULL,
   
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 
 
-INSERT INTO `produtos` (`id`, `nome`, `qnt_voto`) VALUES
-(1, 'Vinho Chileno Seco', 9),
-(2, 'Vinho do Porto', 4),
-(3, 'Vinho Nacional Seco', 15);
-(4, 'Vinho Nacional Suave', 3);
+INSERT INTO `clientes` (`id`, `nome`, `e-mail`, `senha`, `cargo`, `salario`, `cpf`, `rg`, `atribuicoes`, `dt_inicio`, `dt_nasc`) VALUES
+(1, 'joao', 'joao@email.com', 1234, 'recepcionista', 1200, 00000000000, 000000000, 'atender', 2021-01-01, 1999-01-01);
+(2, 'maria', 'maria@email.co'm, 1234, 'recepcionista', 1200, 11111111111, 111111111, 'atender', 2021-04-04, 1998-01-01);
+(3, 'pedro', 'pedro@email.com', 1234, 'gerente', 4000, 22222222222, 222222222, 'gestao', 2017-08-08, 1997-01-01);
+(4, 'ana', 'ana@email.com', 1234, 'coordenador', 4000, 33333333333, 333333333, 'coordenacao', 2021-05-05, 1996-01-01);
 
 
-ALTER TABLE `produtos`
+ALTER TABLE `clientes`
   ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `produtos`
+ALTER TABLE `clientes`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
